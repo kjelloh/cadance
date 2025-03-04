426f0ca2255e5890030d4579ca3267347ebd18f5
kjelloh <kjell-olov.hogdahl@itfied.se>
2025-03-04 16:52:36 +0100

    Added zsh-scripts (macOS) to check for conan package versions and to build+run cadance app in workspace subfolder

---
+++ check_versions.zsh
@@ -0,0 +1,9 @@
+echo Listing of available conan package versions
+conan search opencascade -r=conancenter
+conan search imgui -r=conancenter
+conan search gtest -r=conancenter
+conan search immer -r=conancenter
+conan search pugixml
+conan search spdlog
+echo Compare with conanfile.py requires listing
+cat conanfile.py | grep 'self.requires'
